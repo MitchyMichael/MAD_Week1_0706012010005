04f3b6addbb2812232d3da205fdbfeff266460b4
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2021-09-14 11:18:08 +0700

    Initial commit

---
+++ app/src/main/java/com/progtechuc/mad_week1_0706012010005/MainActivity.java
@@ -0,0 +1,14 @@
+package com.progtechuc.mad_week1_0706012010005;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
