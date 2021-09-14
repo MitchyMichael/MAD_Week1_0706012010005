2987ecf083929fec958d9720670e5c5eca270b4f
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2021-09-14 12:05:11 +0700

    a

--- app/src/main/java/com/progtechuc/mad_week1_0706012010005/MainActivity2.java
+++ app/src/main/java/com/progtechuc/mad_week1_0706012010005/MainActivity2.java
@@ -1,14 +1,19 @@
 package com.progtechuc.mad_week1_0706012010005;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+
+
     }
 }
